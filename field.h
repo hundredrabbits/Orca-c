0c17f593020186f0e19216e1827ad976478ca8fb
cancel <cancel@cancel.fm>
2018-11-26 05:41:53 +0900

    Cleanup arg naming

--- field.h
+++ field.h
@@ -1,21 +1,21 @@
 #pragma once
 #include "base.h"
 
-void field_init(Field* f);
-void field_init_fill(Field* f, Usz height, Usz width, Glyph fill_char);
-void field_resize_raw(Field* f, Usz height, Usz width);
-void field_deinit(Field* f);
+void field_init(Field* field);
+void field_init_fill(Field* field, Usz height, Usz width, Glyph fill_char);
+void field_resize_raw(Field* field, Usz height, Usz width);
+void field_deinit(Field* field);
 void field_copy_subrect(Field* src, Field* dest, Usz src_y, Usz src_x,
                         Usz dest_y, Usz dest_x, Usz height, Usz width);
-void field_fill_subrect(Field* f, Usz y, Usz x, Usz height, Usz width,
+void field_fill_subrect(Field* field, Usz y, Usz x, Usz height, Usz width,
                         Glyph fill_char);
-Glyph field_peek(Field* f, Usz y, Usz x);
-Glyph field_peek_relative(Field* f, Usz y, Usz x, Isz offs_y, Isz offs_x);
-void field_poke(Field* f, Usz y, Usz x, Glyph glyph);
-void field_poke_relative(Field* f, Usz y, Usz x, Isz offs_y, Isz offs_x,
+Glyph field_peek(Field* field, Usz y, Usz x);
+Glyph field_peek_relative(Field* field, Usz y, Usz x, Isz offs_y, Isz offs_x);
+void field_poke(Field* field, Usz y, Usz x, Glyph glyph);
+void field_poke_relative(Field* field, Usz y, Usz x, Isz offs_y, Isz offs_x,
                          Glyph glyph);
 
-void field_fput(Field* f, FILE* stream);
+void field_fput(Field* field, FILE* stream);
 
 typedef enum {
   Field_load_error_ok = 0,
